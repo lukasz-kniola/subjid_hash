6d3935f1ecbfabd42c74e03debf0a3ad0bf46bf9
Lukasz <lukasz.kniola@gmail.com>
2020-02-13 23:54:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+a=[{"USUBJID":'CT1/' + str(i).zfill(3)} for i in range(101,111)]
+dataset = pd.DataFrame(a)
+
+# dataset as Pandas.DataFrame #
+
+import hashlib
+
+def sha_it(s):
+    return hashlib.sha256(s.encode()).hexdigest().upper()
+        
+def hashid(var, key, hashlen):
+    der = sum([ord(j) * (i+1) for i,j in enumerate(key)])
+    strt = der % (64 - hashlen)
+    return (sha_it(key + var))[strt:][:hashlen]
+        
+dataset['DSUBJID'] = dataset['USUBJID'].apply(hashid,args=('C0mpl3t3Ly+r@nd0m=kEY', 8))
+
+print(dataset)
+
+print(dataset.describe())
